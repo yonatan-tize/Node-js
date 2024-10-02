ec86fc5130c0f4688dc665c245f11d954cd7b4c0
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-10-03 00:09:24 +0300

    create(controller): created controller and make routes clean

---
+++ server/controller/userController.js
@@ -0,0 +1,44 @@
+let users = [
+    {id: 1, name: "yr"},
+    {id: 2, name: "yk"},
+    {id: 3, name: "yt"},
+]
+
+export const getUsers = (req, res)=>{
+    res.json(users)
+}
+
+export const getUser = (req, res)=>{
+    const userId = parseInt(req.params.id)
+    res.json(users.filter(user => user.id === userId))
+}
+
+export const postUser = (req, res)=>{
+    const newUSER = {
+        id: users.length,
+        name: req.body
+    }
+    users.push(newUSER)
+
+    res.status(201).json({message: "user created"})
+}
+
+export const putUser = (req, res)=>{
+    //find user
+    const id = parseInt(req.params.id)
+    const user = users.find(user => user.id !== id)
+    if (!user) return res.status(404).json({message: 'user not found'})
+    
+    user.name = req.body.name 
+    res.status(200).json({message: "user updated"})
+}
+
+export const deleteUser = (req, res)=>{
+    const userCount  = users.length
+    const id = parseInt(req.params.id)
+    users = users.filter(user => user.id !== id)
+    if (users.length === userCount) return res.status(404).json({message: 'user not found'})
+
+    res.status(200).json({message: "deleted seccessfully"})
+
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,23 +1,21 @@
 import express from 'express'
+import {getUser, getUsers, postUser, putUser, deleteUser} from "../controller/userController.js"
 
 const router = express.Router()
 
-const users = [
-    {id: 1, name: "yr"},
-    {id: 2, name: "yk"},
-    {id: 3, name: "yt"},
-]
+
 
 // app.use(express.static(path.join(__dirname, 'public')))
 
 //get all users
-router.get('/', (req, res)=>{
-    res.json(users)
-})
+router.get('/', getUsers)
 
-router.get('/:id', (req, res)=>{
-    const userId = parseInt(req.params.id)
-    res.json(users.filter(user => user.id === userId))
-})
+router.get('/:id', getUser)
+
+router.post('/', postUser)
+
+router.put('/:id', putUser)
+
+router.delete('/:id', deleteUser)
 
 export default router
